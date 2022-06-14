d64a59d5f3bd4ad2202845ba730c7e1cb3a6c1ff
July <julwujuly@gmail.com>
2022-06-14 09:57:46 +0500

    documentation route is added

--- app/main.py
+++ app/main.py
@@ -23,3 +23,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get('/')
+def main_page():
+    return {"message": "Read the documentation: https://july-posts-api.herokuapp.com/redoc"}
